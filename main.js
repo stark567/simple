7bfa73c5bb3b0b179f9c6967b568948b5a9b4db2
stark567 <65444467+stark567@users.noreply.github.com>
2020-11-09 13:27:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+burger=document.querySelector('.burger')
+header=document.querySelector('.header')
+right=document.querySelector('.header-right')
+burger.addEventListener('click',()=>{
+    navlist.classList.toggle('v');
+    navbar.classList.toggle('h-nav');
+})
+
