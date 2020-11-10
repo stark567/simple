825804daf27bda26fda43fd2f64cdaa9280f5722
stark567 <65444467+stark567@users.noreply.github.com>
2020-11-10 21:59:20 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-burger=document.querySelector('.burger')
-header=document.querySelector('.header')
-right=document.querySelector('.header-right')
-burger.addEventListener('click',()=>{
-    navlist.classList.toggle('v');
-    navbar.classList.toggle('h-nav');
-})
-
